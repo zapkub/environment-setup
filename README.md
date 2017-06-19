a15a153f86d3fc5e57d01acc351dd8e5639ddb67
Rungsikorn Rungsikavarnich <rungsikornrungsikavarnich@Rungsikorns-MacBook-Pro.local>
2017-06-19 23:21:03 +0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+
+## VSCode
+- install Setting sync plugin and use: `a0289fff1f8e564359a7da6871c9ee73` as gist id
---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
+
+echo "Installing brew..."
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+echo 'Installing oh-my-zsh'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew install zsh zsh-completions
+
+echo 'Installing iterm2....'
+brew cask install iterm2
+
+# Editor
+brew install neovim/neovim/neovim
+brew cask install caskroom/cask/visual-studio-code
+
+# Dev tools
+brew cask install robomongo
+brew cask search postman
+brew cask install graphiql
+
+# Browser
+brew cask install google-chrome
+brew cask install firefox
+
+# Messenger
+brew cask install telegram
+brew cask install slack
+
+# Utilities
+brew cask install sourcetree
+brew cask install dropbox
+brew cask install transmit
+brew cask install evernote
+brew cask install parallels-desktop
+brew cask install microsoft-office
+brew cask install duet
+
+# Design
+brew cask install sketch
+brew cask install adobe-photoshop-cc
+brew cask install adobe-media-encoder-cc
